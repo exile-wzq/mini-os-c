aa62bc138f9791bee7c6efa8cbd5783ab00a787d
Exile <1186901999@qq.com>
2026-03-11 05:26:48 +0800

    修正

--- mini-os-c/include/cpu.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by 11869 on 2026/3/11.
-//
-
-#ifndef C_PROJECT_CPU_H
-#define C_PROJECT_CPU_H
-
-#endif //C_PROJECT_CPU_H
--- mini-os-c/include/isa.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by 11869 on 2026/3/11.
-//
-
-#ifndef C_PROJECT_ISA_H
-#define C_PROJECT_ISA_H
-
-#endif //C_PROJECT_ISA_H
